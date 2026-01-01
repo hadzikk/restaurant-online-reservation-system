a119bfc6d4407f282fdad1b0f4d026bc2990b0b0
hadzikk <hadzikmochammad@gmail.com>
2026-01-01 22:37:07 +0700

    feat(table details): show all table details from database

--- packages/domains/customer/src/components/TableDetails/TableDetails.tsx
+++ packages/domains/customer/src/components/TableDetails/TableDetails.tsx
@@ -5,23 +5,37 @@ import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
 import styles from './TableDetails.module.css'
 
 interface Table {
-  id: number
-  x: number
-  y: number
-  width: number
-  height: number
   table_code: string
   status: string
 }
 
 interface TableDetails {
-  id: number
   capacity: number
   description: string | null
-  location_id: number
-  table_id: number
   table_size_id: number
   restaurant_tables: Table
+  restaurant_table_locations: TableLocation
+  restaurant_table_sizes: TableSize
+  restaurant_table_features: TableFeatures[]
+}
+
+interface TableLocation {
+    id: number
+    name: string
+}
+
+interface TableSize {
+    name: string
+    description: string
+}
+
+interface TableFeatures {
+    name: string
+    description: string
+}
+
+interface TableLocation {
+    name: string
 }
 
 const TablePreview = () =>  {
@@ -34,7 +48,10 @@ const TablePreview = () =>  {
             .from('restaurant_table_details')
             .select(`
                 *,
-                restaurant_tables(table_code)
+                restaurant_tables(table_code, status),
+                restaurant_table_locations(name),
+                restaurant_table_sizes(name),
+                restaurant_table_features(name, description)
             `)
             .eq('id', 1)
             .single()
@@ -59,8 +76,13 @@ const TablePreview = () =>  {
                 />
             </figure>
             <div className="container">
-                <p className={styles.header}>table</p>
-                <p className={styles.tableCode}>{table?.restaurant_tables?.table_code}</p>
+                <div className={styles.tableInfo}>
+                    <div className={styles.tableHeader}>
+                        <p className={styles.header}>table</p>
+                        <p className={styles.tableCode}>{table?.restaurant_tables?.table_code}</p>
+                    </div>
+                    <span className={styles.status}>{table?.restaurant_tables?.status}</span>
+                </div>
                 <details className={styles.detailItem}>
                     <summary className={styles.detailSummary}>
                         <span className={styles.detailTitle}>description</span>
@@ -76,7 +98,7 @@ const TablePreview = () =>  {
                 </details>
                 <details className={styles.detailItem}>
                     <summary className={styles.detailSummary}>
-                        <span className={styles.detailTitle}>characteristics</span>
+                        <span className={styles.detailTitle}>about this table</span>
                         <span className={styles.arrow}>
                             <FontAwesomeIcon
                                 icon={faAngleUp}
@@ -94,12 +116,44 @@ const TablePreview = () =>  {
                             <tbody>
                                 <tr>
                                     <td>Size</td>
-                                    <td>{table?.restaurant_tables?.width} x {table?.restaurant_tables?.height}</td>
+                                    <td>{table?.restaurant_table_sizes?.name}</td>
                                 </tr>
                                 <tr>
                                     <td>Capacity</td>
                                     <td>{table?.capacity} people</td>
                                 </tr>
+                                <tr>
+                                    <td>Place</td>
+                                    <td>{table?.restaurant_table_locations?.name}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </details>
+                <details className={styles.detailItem}>
+                    <summary className={styles.detailSummary}>
+                        <span className={styles.detailTitle}>features</span>
+                        <span className={styles.arrow}>
+                            <FontAwesomeIcon
+                                icon={faAngleUp}
+                            />
+                        </span>
+                    </summary>
+                    <div className={styles.detailContent}>
+                        <table className={styles.table}>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {table?.restaurant_table_features.map((feature) => (
+                                    <tr>
+                                        <td>{feature.name}</td>
+                                        <td>{feature.description}</td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
