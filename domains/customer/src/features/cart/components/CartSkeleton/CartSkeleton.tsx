c1ff65af118aad9b6fd3628fe998df6f836dfef9
hadzikk <hadzikmochammad@gmail.com>
2026-01-08 21:04:52 +0700

    feat(cart): add cart skeleton loader and it's close event

---
+++ domains/customer/src/features/cart/components/CartSkeleton/CartSkeleton.tsx
@@ -0,0 +1,27 @@
+// CartSkeleton.tsx
+import React from 'react'
+import styles from './CartSkeleton.module.css'
+
+interface CartSkeletonProps {
+  className?: string;
+}
+
+const CartSkeleton: React.FC<CartSkeletonProps> = ({ className = '' }) => {
+    return (
+        <div className={`${styles.root} ${className}`.trim()}>
+            <div className={styles.skeletonTitle}></div>
+            <div className={styles.skeletonTitle}></div>
+            <div className={styles.skeletonTable}></div>
+            <div className={styles.skeletonTitle}></div>
+            <div className={styles.skeletonMenu}></div>
+            <div className={styles.skeletonTitle}></div>
+            <div className={styles.skeletonTotal}></div>
+            <div className={styles.skeletonControls}>
+                <div className={styles.skeletonButton}></div>
+                <div className={styles.skeletonButton}></div>
+            </div>
+        </div>
+    )
+}
+
+export default CartSkeleton
